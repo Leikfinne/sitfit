c48e607517fa903c69e827379894b05112127390
Ludvig Jordet <ludvig.jordet@gmail.com>
2017-01-27 18:20:07 +0100

    create function for sending arbitrary large array of ints over Wire

--- arduino_i2c_slave/arduino_i2c_slave.ino
+++ arduino_i2c_slave/arduino_i2c_slave.ino
@@ -1,22 +1,32 @@
 #include <Wire.h>
 
+#define I2C_ADDRESS 8
+#define SEND_BUFFER_SIZE 128
+#define NUM_SENSORS 1
+
 void setup() {
-  Wire.begin(8);
+  Wire.begin(I2C_ADDRESS);
   Wire.onRequest(requestEvent);
-  Serial.begin(115200);
 }
 
 void loop() {
   delay(100);
 }
 
-byte message[2];
-int sensor_value;
+int16_t sensor_values[NUM_SENSORS];
 
 void requestEvent() {
-  sensor_value = analogRead(A0);
-  message[0] = (sensor_value >> 8) & 0xFF;
-  message[1] = sensor_value & 0xFF;
-  Wire.write(message, 2);
+  sensor_values[0] = analogRead(A0);
+  send_int16_ts(sensor_values, 1);
+}
+
+byte message[SEND_BUFFER_SIZE];
+void send_int16_ts(int16_t *values, unsigned int num){
+  int i;
+  for(i = 0; i < num; ++i){
+    message[2*i] = (values[i] >> 8) & 0xFF;
+    message[2*i + 1] = values[i] & 0xFF;
+  }
+  Wire.write(message,num*2);
 }
 
